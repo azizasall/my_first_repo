6dd3b5b63409d29cb646f28d7651e22eab87572f
azizasall <elhadji-abdou-aziz.sall.1@ulaval.ca>
2020-10-11 23:59:09 -0400

    ajout calcul des rendements

--- Test_with_mtcars_data.R
+++ Test_with_mtcars_data.R
@@ -2,7 +2,7 @@ rm(list=ls())
 
 library(tseries)
 
-data()
+# data()
 
 my_data <- mtcars
 my_data
@@ -10,5 +10,21 @@ my_data
 
 my_data$mpg 
 
+
+split.screen(c(2,1))
+screen(1)
 plot(ts(my_data$mpg))
+
+
+screen(2)
+plot(ts(my_data$hp))
+
+
+
+# test avec git si je change quelque chose dans le fichier
+
+ret <- diff(log(my_data$mpg))
+
+
+
 
